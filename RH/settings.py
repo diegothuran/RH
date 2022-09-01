7adf048d149880364b27144120448f7d6b440ec7
Thuran <diego.thuran@gmail.com>
2022-09-01 13:44:25 -0300

    creat departaments app create departaments model migrate

--- RH/settings.py
+++ RH/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.companies',
-    'apps.employes'
+    'apps.employes',
+    'apps.departaments'
 ]
 
 MIDDLEWARE = [
